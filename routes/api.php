58d47d2bf8816484f592b5123b37546ae70b1ccd
Wesley Hott Soares <wesley@mpmg.mp.br>
2023-01-07 15:21:03 -0300

    chore #235 - Including test files

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+/* >>>> LARAVUE */
+
+Route::apiResources([
+	// {{ laravue-insert:route }}
+]);
+/* <<<< LARAVUE */
